efcd0d22bf3276213755a725b2da1ee352313dea
Hanin-suwaid <144672778+haninsuwaid@users.noreply.github.com>
2023-12-18 10:06:54 +0100

    image veranderd/ een bestand gemaakt voor de functies
    Olivier en ik hebben de search van de image aangepast. We hebben ook een folder aangemaakt waar we de functies in gaan zetten.

---
+++ Statistiekalgoritmen/Statistiekalgoritmen.py
@@ -0,0 +1,7 @@
+"""
+    Hier gaan wij een lege functie maken met parameters, en de return. Daarbij gaan we ook uitleg
+    schrijven over wat de functie doet, waar de parameters voor gebruikt worden,
+    en wat de functie uiteindelijk returnt. Alleen dus Statistiekalgoritmen van
+    de verplichte onderdelen van Ai.
+"""
+
