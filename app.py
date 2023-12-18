005585b4a1c477d9be4d8823489b624ee0a23b3d
Hanin-suwaid <144672778+haninsuwaid@users.noreply.github.com>
2023-12-18 12:19:21 +0100

    eerste game ophalen functie in het project gezet
    Olivier en ik hebben de Fucntie die Beau had geschreven in flask geimplementeerd. Nu krijgt de gebruiker de eerste game te zien met daarbij de naam prijs en genres.

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
 from flask import Flask, render_template
 from views import views
+import pandas as pd
+import json
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/views")
 
 
+def laad_json_bestand():
+    with open('json/steam.json') as bestand:
+        data = json.load(bestand)
+
+    df = pd.json_normalize(data,
+                           meta=["appid", "name", "name", "english", "developer", "publisher", "platforms",
+                                 "required_age", "categories", "genres", "steamspy_tags",
+                                 "achievements", "positive_ratings", "negative_ratings", "average_playtime",
+                                 "median_playtime", "owners", "price"])
+    return df
+
+def laad_eerste_game(df):
+    data = df
+    return data.iloc[0, :]
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    df = laad_json_bestand()
+    eerste_game = laad_eerste_game(df)
+    return render_template('home.html', eerste_game=eerste_game)
 
-
-@app.route('/home')
-def home2():
-    return 'Test'
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 if __name__ == '__main__':
