fd305c8ee2a85fe426ee2b4a453e0f25e35124fa
Olivier Oostermann <olivier.oostermann@student.hu.nl>
2023-12-20 15:02:41 +0100

    Wij hebben bestanden gelinked
    Hanin en ik hebben de bestanden goed met elkaar gelinked. Nu kunnen wij in het algoritmen bestand

---
+++ Statistiekalgoritmen/algoritmen.py
@@ -0,0 +1,77 @@
+"""
+    Hier gaan wij een lege functie maken met parameters, en de return. Daarbij gaan we ook uitleg
+    schrijven over wat de functie doet, waar de parameters voor gebruikt worden,
+    en wat de functie uiteindelijk returnt. Alleen dus Statistiekalgoritmen van
+    de verplichte onderdelen van Ai.
+"""
+
+import pandas as pd
+import json
+import os
+from flask import Flask, render_template
+
+json_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../..', 'puntcomma', 'json', 'steam.json')
+def laad_json_bestand():
+    with open(json_path) as bestand:
+        data = json.load(bestand)
+
+    df = pd.json_normalize(data,
+                           meta=["appid", "name", "name", "english", "developer", "publisher", "platforms",
+                                 "required_age", "categories", "genres", "steamspy_tags",
+                                 "achievements", "positive_ratings", "negative_ratings", "average_playtime",
+                                 "median_playtime", "owners", "price"])
+    return df
+
+
+def laad_eerste_game(df):
+    data = df
+    return data.iloc[0, :]
+
+
+def sorteer_data(data, column, ascending_bool):
+    sorted_df = data.sort_values(by=(column), ascending=(ascending_bool))
+    return sorted_df
+
+
+"""
+    functie beschrijving:
+        De functie gaat de gemiddelde uitrekenen van de reviews van games
+
+    parameters:
+        data: data die mee wordt uitgerekend om de gemiddelde uit te halen.
+
+    return:
+        De gemiddelde van game review
+"""
+# def kwantitatief_gemiddelde_reviews(positieve_reviews, negatieve_reviews):
+
+    #return gemiddelde_review
+
+
+"""
+    functie beschrijving:
+        De functie gaat de frequentie berekenen van de genres, om te zien
+        hoevaak elke genre het meest voorkomen in games
+
+    parameters:
+        genres: de genres die worden opgehaald.
+
+    return:
+        De frequentie van elke genre
+"""
+#def kwatitatief_frequentie_genres(genres):
+#return genre_frequentie
+
+
+"""
+    functie beschrijving:
+
+
+    parameters:
+
+
+    return: frequentie_prijs
+"""
+# def kwantitatief_frequentie_prijs():
+
+    #return frequentie_prijs
--- app.py
+++ app.py
@@ -1,35 +1,20 @@
 from flask import Flask, render_template
+from Statistiekalgoritmen.algoritmen import *
 from views import views
 import pandas as pd
 import json
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
 app.register_blueprint(views, url_prefix="/views")
-
-
-def laad_json_bestand():
-    with open('json/steam.json') as bestand:
-        data = json.load(bestand)
-
-    df = pd.json_normalize(data,
-                           meta=["appid", "name", "name", "english", "developer", "publisher", "platforms",
-                                 "required_age", "categories", "genres", "steamspy_tags",
-                                 "achievements", "positive_ratings", "negative_ratings", "average_playtime",
-                                 "median_playtime", "owners", "price"])
-    return df
-
-def laad_eerste_game(df):
-    data = df
-    return data.iloc[0, :]
 
 @app.route('/')
 def home():
     df = laad_json_bestand()
     eerste_game = laad_eerste_game(df)
-    return render_template('home.html', eerste_game=eerste_game)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    sorteer_data_data = sorteer_data(df, 'negative_ratings', True)
+    return render_template('home.html', eerste_game=eerste_game, sorteer_data_data=sorteer_data_data)
 
 
 if __name__ == '__main__':
