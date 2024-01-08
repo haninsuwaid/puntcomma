baebbaea721817eb4df821ca4ca9205136d2b2a5
beaugunther <beau.gunther@student.hu.nl>
2024-01-08 16:43:03 +0100

    Functie freq,modi en rapportcijfer gerealiseerd.

--- Statistiekalgoritmen/Statistiekalgoritmen.py
+++ Statistiekalgoritmen/Statistiekalgoritmen.py
@@ -4,31 +4,94 @@
     en wat de functie uiteindelijk returnt. Alleen dus Statistiekalgoritmen van
     de verplichte onderdelen van Ai.
 """
+import json
 
+with open('steam.json', 'r') as bestand:
+    gegevens = json.load(bestand)
 
-
-def kwantitatief_rapportcijfer_reviews(positive_reviews, negative_reviews):
+def freq(lst,key):
     """
-        functie beschrijving:
-            De functie berekend het rapportcijfer per game obv de positieve en negatieve reviews
+        Functie beschrijving:
+            De functie bepaalt hoe vaak de waarde van een element in de lijst voorkomt.
+
+        Parameters:
+            Lst: Lijst met dicts
+            Key: Key waarop bepaalt wordt hoe vaak de waarde in de gegevens lijst voorkomt.
+
+        Return:
+            Dict met de frequentie per value.
+    """
+    freqs = dict()
+
+    for item in lst:
+        if item[key] in freqs:
+            freqs[item[key]] += 1
+        else:
+            freqs[item[key]] = 1
+
+    return freqs
+
+
+def modi(lst,key):
+    """
+        Functie beschrijving:
+            De functie bepaalt welke waarde het meest in de lijst voorkomt.
 
         parameters:
-           positive_reviews: Het aantal positieve beoordelingen.
-           negative_reviews: Het aantal negatieve beoordelingen.
+            Lst: Lijst met dicts
+            Key: Key die doorgegeven dient te worden aan de functie die de frequentie bepaalt
 
-        return:
-            Het rapportcijfer van de reviews per game.
+        Return:
+            Dict met de modus/modi
     """
-    rapportcijfer = (positive_reviews / (positive_reviews + negative_reviews)) * 10 #bijv: 75 / ( 75 + 25) * 10 = 7.5
-    #Kan eventueel ook op een schaal van 5, ipv rapportcijfer aantal sterren.
 
-    return round(rapportcijfer, 1)
+    lijst = freq(lst,key)
+    modi = dict()
+    max_freq = 0
+    for key, value in lijst.items():
+        if value > max_freq:
+            max_freq = value
 
-print(kwantitatief_gemiddelde_reviews(data_list['positive_ratings'],data_list['negative_ratings']))#data dient nog in variabel gezet te worden
-#functies voor mediaan van alle positieve, negatieve en rapportcijfer kunnen. Tevens kan obv hiervan nog de modus of modi vastgesteld worden
+    for key, value in lijst.items():
+        if value >= max_freq:
+            modi[key] = value
+
+    return modi
+
+print(modi(gegevens,'required_age'))
 
 
+def kwantitatief_rapportcijfer_reviews(lst):
+    """
+        Functie beschrijving:
+            De functie berekend de waardering obv het totaal aantal gegeven negatieve en positieve reviews.
+            De waardering wordt uitgedrukt op een schaal van 10.
 
+        Parameters:
+            lst: Lijst van dictionaries.
+
+        Return:
+            Lijst bestaande uit zowel het rapportcijfer als het aantal gegeven ratings per game.
+    """
+
+    cijfers = []
+
+    for item in lst:
+        positieve_reviews = item['positive_ratings']
+        negatieve_reviews = item['negative_ratings']
+        totaal = positieve_reviews + negatieve_reviews
+        rapportcijfer = (positieve_reviews / (totaal)) * 10  # bijv: 75 / ( 75 + 25) * 10 = 7.5
+        game_info = {
+            'Naam': item['name'],
+            'Rapportcijfer': round(rapportcijfer, 1),
+            'Totaal_ratings': totaal
+        }
+        cijfers.append(game_info)
+
+
+    return cijfers
+
+# print(kwantitatief_rapportcijfer_reviews(gegevens))
 """
     functie beschrijving:
         De functie gaat de frequentie berekenen van de genres, om te zien
@@ -55,5 +118,3 @@ def kwatitatief_frequentie_genres(genres):
     """
 def kwantitatief_frequentie_prijs():
     # return gemiddelde_review
-
-
