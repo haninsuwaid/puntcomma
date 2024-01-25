1d647cee67d9f73fdf775ca4b0bc6e82dc73cdc8
Hanin-suwaid <144672778+haninsuwaid@users.noreply.github.com>
2024-01-25 21:42:45 +0100

    session, check voor session
    De key en de appid die de gebruiker invult in de landingspage die worden opgeslagen in de url en worden daarna gebruikt voor de fucnties waar de gegevens van de gebruiker worden opgehaald. Ik heb ook sessie gestart die de key en de steamid van de gebruiker opslaat.

--- Statistiekalgoritmen/apiJson.py
+++ Statistiekalgoritmen/apiJson.py
@@ -14,9 +14,9 @@ def get_json_api(api, *keys):
     result = get_api_key(api_json_data, keys)
     return result
 
-def user():
+def user(key, steamid):
     user = get_json_api(
-        "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=B129420016573EE260056E21D4218C90&steamids=76561198366424343",
+        f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={key}&steamids={steamid}",
         "response", "players")
     for data in user:
         persona_name = data["personaname"]
@@ -27,23 +27,9 @@ def user():
     return data
 
 
-def all_owned_games():
+def amount_owned_games(key, steamid):
     games = get_json_api(
-        "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=B129420016573EE260056E21D4218C90&steamid=76561198366424343&format=json",
-        "response", "games"
-    )
-    appids = []
-    for owned_games_data in games:
-        if owned_games_data["playtime_forever"] > 0:
-            appid = owned_games_data["appid"]
-            appids.append(appid)
-
-    return appids
-
-
-def amount_owned_games():
-    games = get_json_api(
-        "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=B129420016573EE260056E21D4218C90&steamid=76561198366424343&format=json",
+        f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={key}&steamid={steamid}&format=json",
         "response"
     )
     return games
@@ -69,9 +55,10 @@ def steam_game_info(gameid):
     return game_info
 
 
-def all_owned_games():
+
+def all_owned_games(key, steamid):
     games = get_json_api(
-        "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=B129420016573EE260056E21D4218C90&steamid=76561198366424343&format=json",
+        f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={key}&steamid={steamid}&format=json",
         "response", "games"
     )
     appids = []
@@ -79,8 +66,21 @@ def all_owned_games():
         if owned_games_data["playtime_forever"] > 0:
             appid = owned_games_data["appid"]
             appids.append(appid)
+
     return appids
 
+
+def owned_games_info(key, steamid, limit=0):
+    all_owned_games_ids = all_owned_games(key, steamid)
+    all_game_info = []
+    for index, appid in enumerate(all_owned_games_ids[:limit]):
+        game_info = get_json_api(
+            f"https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid={appid}&key={key}&steamid={steamid}",
+            "playerstats"
+        )
+        if "gameName" in game_info and game_info["gameName"]:
+            all_game_info.append(game_info)
+    return all_game_info
 
 def friends_steam_id():
     friends = get_json_api(
@@ -108,21 +108,6 @@ def friends_list_info(limit=0):
     return all_friends_info
 
 
-def owned_games_info(limit=0):
-    all_owned_games_ids = all_owned_games()
-    all_game_info = []
-    for index, appid in enumerate(all_owned_games_ids[:limit]):
-        game_info = get_json_api(
-            f"https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid={appid}&key=B129420016573EE260056E21D4218C90&steamid=76561198366424343",
-            "playerstats"
-        )
-        if "gameName" in game_info and game_info["gameName"]:
-            all_game_info.append(game_info)
-    return all_game_info
-
-
-print(friends_list_info(limit=1))
-print(owned_games_info(limit=1))
 
 
 
