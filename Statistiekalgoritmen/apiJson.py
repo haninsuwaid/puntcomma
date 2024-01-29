3df65e31622cfe581fe36296e2e1cd277d9d2328
Hanin-suwaid <144672778+haninsuwaid@users.noreply.github.com>
2024-01-30 00:15:20 +0100

    comments, deleted files
    Ik heb commentaar geschreven bij alle functies die ik heb geschreven en ik heb files verwijderd die niet gebruikt worden

--- Statistiekalgoritmen/apiJson.py
+++ Statistiekalgoritmen/apiJson.py
@@ -1,15 +1,32 @@
 import requests
-import datetime
 import random
 
 
 def get_api_key(data, keys):
+    """
+        Functie beschrijving:
+            loop to access nested keys
+        Parameters:
+            data: for the api link
+            keys: for the keys of the api
+        Return:
+            data: the last key or if not found an empty dict
+    """
     for key in keys:
         data = data.get(key, {})
     return data
 
 
 def get_json_api(api, *keys):
+    """
+        Functie beschrijving:
+            this function is to make api calls
+        Parameters:
+            api: the api call link
+            *keys: the keys of the api call the * means it could have multiple keys
+        Return:
+            result: the whole api call with the link and the key/s needed
+    """
     response = requests.get(api)
     api_json_data = response.json()
     result = get_api_key(api_json_data, keys)
@@ -17,25 +34,159 @@ def get_json_api(api, *keys):
 
 
 def user(key, steamid):
+    """
+        Functie beschrijving:
+            Api to get player's info with their key and steamid
+        Parameters:
+            key: the key the user gets from steam to get their own api link
+            steamid: the steamid the user gets from using steam
+        Return:
+            dataset of users data
+    """
     user = get_json_api(
         f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={key}&steamids={steamid}",
         "response", "players")
-
     return user
+
 
 def user_by_id(key, steamid):
-    user = get_json_api(
+    """
+        Functie beschrijving:
+
+        Parameters:
+
+        Return:
+
+    """
+    user_id = get_json_api(
         f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={key}&steamids={steamid}",
         "response", "players")
 
-    return user
+    return user_id
+
+
+def steam_game_info(gameid):
+    """
+        function description:
+            This function returns information of the chosen steam game
+
+        parameters:
+            gameid: The id that chooses which games information will be shown
+
+        return: game_info
+    """
+    game_info = get_json_api(f"https://store.steampowered.com/api/appdetails?appids={gameid}")
+    return game_info
+
 
 def amount_owned_games(key, steamid):
+    """
+        Functie beschrijving:
+            this function gets the amout of owned games from owned games api it get the first key.
+        Parameters:
+            key: the key the user gets from steam to get their own api link
+            steamid: the steamid the user gets from using steam
+        Return:
+            the key where the amount of games the user has is mentioned
+    """
     games = get_json_api(
         f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={key}&steamid={steamid}&format=json",
         "response"
     )
     return games
+
+
+def all_owned_games(key, steamid):
+    """
+        Functie beschrijving:
+            this function gets all owned games of the logged-in user and the appids. And it sorts games
+            based of the playtime. From most played to least played
+        Parameters:
+            key: the key the user gets from steam to get their own api link
+            steamid: the steamid the user gets from using steam
+        Return:
+            it returns the appids of the owned games if the playtime is larger then 0
+    """
+    games = get_json_api(
+        f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={key}&steamid={steamid}&format=json",
+        "response", "games"
+    )
+    appids = []
+    sorted_games = sorted(games, key=lambda x: x.get("playtime_forever"))
+    for owned_games_data in sorted_games:
+        if owned_games_data["playtime_forever"] > 0:
+            appid = owned_games_data["appid"]
+            appids.append(appid)
+    return appids
+
+
+def owned_games_info(key, steamid, limit=0):
+    """
+        Functie beschrijving:
+            this function get the all_owned_games function that has all appids of owned games with playtime
+            it limits the amount of games I want to get. It also checks if the key gamename exists and is not empty
+        Parameters:
+            key: the key the user gets from steam to get their own api link
+            steamid: the steamid the user gets from using steam
+            limit=0:
+        Return:
+            games info with gameName
+    """
+    all_owned_games_ids = all_owned_games(key, steamid)
+    all_game_info = []
+    for index, appid in enumerate(all_owned_games_ids[:limit]):
+        game_info = get_json_api(
+            f"https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid={appid}&key={key}&steamid={steamid}",
+            "playerstats"
+        )
+        if "gameName" in game_info and game_info["gameName"]:
+            all_game_info.append({"appid": appid, "game_info": game_info})
+    return all_game_info
+
+
+def friends_steam_id(key, steamid):
+    """
+        Functie beschrijving:
+            this function gets friends steam id to use the id in another function where i get their data
+            with the steamids. This function sorts friends from the oldest to newest friends.
+        Parameters:
+            key: the key the user gets from steam to get their own api link
+            steamid: the steamid the user gets from using steam
+        Return:
+            a list of steamids
+    """
+    friends = get_json_api(
+        f"https://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={key}&steamid={steamid}&relationship=friend",
+        "friendslist", "friends"
+    )
+    friend_list_data = []
+    sorted_friends = sorted(friends, key=lambda x: x.get("friend_since"))
+    for friend in sorted_friends[:10]:
+        steam_id = friend["steamid"]
+        friend_list_data.append(steam_id)
+    return friend_list_data
+
+
+def friends_list_info(key, steamid, limit=0):
+    """
+        Functie beschrijving:
+            a function to get the steam ids from the another function and get the data of the friends.
+        Parameters:
+            key: the key the user gets from steam to get their own api link
+            steamid: the steamid the user gets from using steam
+            limit=0: a limit to limit the amout of friends i want to get
+        Return:
+            a list of friends info
+    """
+    friends_list_steam_id = friends_steam_id(key, steamid)
+    all_friends_info = []
+    for index, steamid in enumerate(friends_list_steam_id[:limit]):
+        friend_info = get_json_api(
+            f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={key}&steamids={steamid}",
+            "response", "players"
+        )
+        all_friends_info.append(friend_info)
+    return all_friends_info
 
 
 def all_steam_games(limit=0):
@@ -48,7 +199,6 @@ def all_steam_games(limit=0):
 
         return: gamelist
     """
-
     # Get the games out of the api
     games = get_json_api("https://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json")
     apps = games["applist"]["apps"]
@@ -69,19 +219,6 @@ def all_steam_games(limit=0):
     return gamelist
 
 
-def steam_game_info(gameid):
-    """
-        function description:
-            This function returns information of the chosen steam game
-
-        parameters:
-            gameid: The id that chooses which games information will be shown
-
-        return: game_info
-    """
-    game_info = get_json_api(f"https://store.steampowered.com/api/appdetails?appids={gameid}")
-    return game_info
-
 def info_for_steam_games():
     """
         function description:
@@ -96,62 +233,3 @@ def info_for_steam_games():
     # To get all the data from the random chosen steam game, put it in a var and return the game_inf_steam
     game_info_steam = [steam_game_info(game["appid"]) for game in list_of_games]
     return game_info_steam
-
-
-def all_owned_games(key, steamid):
-    games = get_json_api(
-        f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={key}&steamid={steamid}&format=json",
-        "response", "games"
-    )
-    appids = []
-    sorted_games = sorted(games, key=lambda x: x.get("playtime_forever"))
-    for owned_games_data in sorted_games:
-        if owned_games_data["playtime_forever"] > 0:
-            appid = owned_games_data["appid"]
-            appids.append(appid)
-    return appids
-
-
-def owned_games_info(key, steamid, limit=0):
-    all_owned_games_ids = all_owned_games(key, steamid)
-    all_game_info = []
-    for index, appid in enumerate(all_owned_games_ids[:limit]):
-        game_info = get_json_api(
-            f"https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid={appid}&key={key}&steamid={steamid}",
-            "playerstats"
-        )
-        if "gameName" in game_info and game_info["gameName"]:
-            all_game_info.append({"appid": appid, "game_info": game_info})
-    return all_game_info
-
-
-def friends_steam_id(key, steamid):
-    friends = get_json_api(
-        f"https://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={key}&steamid={steamid}&relationship=friend",
-        "friendslist", "friends"
-    )
-    friend_list_data = []
-    sorted_friends = sorted(friends, key=lambda x: x.get("friend_since"))
-    for friend in sorted_friends[:10]:
-        steam_id = friend["steamid"]
-        friend_list_data.append(steam_id)
-    return friend_list_data
-
-
-def friends_list_info(key, steamid, limit=0):
-    friends_list_steam_id = friends_steam_id(key, steamid)
-    all_friends_info = []
-    for index, steamid in enumerate(friends_list_steam_id[:limit]):
-        friend_info = get_json_api(
-            f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={key}&steamids={steamid}",
-            "response", "players"
-        )
-        all_friends_info.append(friend_info)
-    return all_friends_info
-
-
-
-
-
-
-
--- app.py
+++ app.py
@@ -10,8 +10,17 @@ app.register_blueprint(views, url_prefix="/views")
 
 app.secret_key = 'geheim'
 
+
 @app.route('/', methods=['GET', 'POST'])
 def landing_page():
+    """
+        Functie beschrijving:
+            this function sends the user to the landingspage where the user has to fill their key and steamid
+            after they log in a session starts. with the POST method. The function gets the key and steamid
+            from the form. After that the user get sent to the profile page.
+        Return:
+            the landingspage
+    """
     if request.method == 'POST':
         session['steamid'] = request.form['steamid']
         session['key'] = request.form['key']
@@ -21,6 +30,15 @@ def landing_page():
 
 @app.route('/profile/', methods=['GET', 'POST'])
 def profile():
+    """
+        Functie beschrijving:
+            this fucntion first checks if there is a POST request from the form if yes it gets the key and
+            steam id and uses it for the session. If the request is not compliet the user get sent to the
+            landingspage.
+            another function needed for the profile page are also called from app.py.
+        Return:
+            the profile page with the called functions for that page
+    """
     if request.method == 'POST':
         steamid = request.form['steamid']
         key = request.form['key']
@@ -40,6 +58,12 @@ def profile():
 
 @app.route('/home/')
 def home():
+    """
+        Functie beschrijving:
+            this function gets functions from another files to display the returned data.
+        Return:
+            the page path and the needed functions for that page
+    """
     df = laad_json_bestand()
     eerste_game = laad_eerste_game(df)
     sorteer_data_data = sorteer_data(df, 'negative_ratings', True)
@@ -49,24 +73,46 @@ def home():
 
 @app.route('/game/<appid>')
 def game(appid):
+    """
+        Functie beschrijving:
+            a function that gets the appid of a chosen game and the fucntion steam_game_info uses the id
+            to get the game data with the function.
+        Parameters:
+            appid: the id of a chosen game from the url
+        Return:
+            the path of the game page and the function
+    """
     games_data = steam_game_info(appid)
     return render_template('game.html', game=games_data)
 
 
 @app.route('/stats/')
 def stats():
+    """
+        Functie beschrijving:
+            fucntion to render the stats.html page
+        Return:
+            returns the path of that page
+    """
     return render_template('stats.html')
 
 
 @app.route('/owned_games/')
 def owned_games():
+    """
+        Functie beschrijving:
+            this function gets the key and steamid from the session. and it gets the appid.
+            So they could be used in the parameters of the owned_games_info function.
+        Return:
+            the path of the owned_game page with the needed fucntions in that page
+    """
     key = session.get('key')
     steamid = session.get('steamid')
     appid = request.args.get('appid')
-    all_steam_game = all_steam_games(limit=15)
     game_name = owned_games_info(key, steamid, limit=15)
     game_info = info_for_steam_games()
     return render_template('owned_games.html', appid=appid, game_info=game_info, game_name=game_name)
+
 
 #rick's meuk
 @app.route("/testdata")
@@ -74,11 +120,11 @@ def testdata():
     a = {"key": "value"}
     return jsonify(a)
 
+
 @app.route("/test_games/<key>/<user_id>", methods = ['POST'])
 def test_games(key, user_id):
     user_profile = user_by_id(key, user_id)
     return jsonify(user_profile)
-
 
 
 if __name__ == '__main__':
