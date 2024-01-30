d58cbfdf720874a80f5606a36997f86e7a86c43c
h8f5y2 <144606226+h8f5y2@users.noreply.github.com>
2024-01-30 11:33:12 +0100

    Mergesort functiebeschrijving aangepast.

--- Statistiek_algoritmen/merge_sort.py
+++ Statistiek_algoritmen/merge_sort.py
@@ -5,17 +5,33 @@ import time
 def mergesort_split(lst, value):
     """
     Functiebeschrijving:
-        De functie sorteert een lijst met behulp van het mergesort-algoritme, dat de "divide en qonquer" methode gebruikt.
+        De functie sorteert een lijst met behulp van het mergesort-algoritme, dat gebruikt maakt van de "divide en qonquer" methode.
         Het algoritme splits de lijst op in kleinere delen tot elk deel slechts een element bevat en voegt vervolgens recursief de gesorteerde delen samen.
+
+        Onderstaande stappen beschrijven summier welke stappen het algoritme maakt:
+        • De lijst wordt in tweeën gesplitst totdat er niets meer te splitsen valt.
+        • Vervolgens worden de afzonderlijke delen recursief met elkaar vergeleken.
+        • Voor elke lijst wordt een teller bijgehouden.
+        • Bij elke vergelijking wordt de voorwaarde gesteld dat de teller kleiner is dan de lengte van de lijst.
+        • Bij elke wisseling wordt er een teller bijgehouden, zodat duidelijk is op welke plaats het item neergezet dient te worden.
+        •
+        • Als voorbeeld:
+        • 5 en 4 worden met elkaar vergeleken.
+        • Als 5 < 4, wat onwaar is, gaat de code door naar het 'else' statement waar 4 op zijn plek wordt gezet en de teller voor deze lijst toeneemt.
+        • Het overgebleven item 5 wordt op zijn plek gezet, waarvan de teller ook toeneemt.
+        • Na de volgende iteratie zal deze teller 1 zijn, en wordt er niet meer aan de voorwaarde voldaan dat de teller kleiner is dat de lengte van de lijst.
+        • De lus verbreekt, waarna het volgende onderdeel recursief wordt opgelost.
 
     Parameters:
         Lst: Lijst die gesorteerd dient te worden.
-        Value: Meegegeven waarde waar op gesorteerd dient te worden.
+        Value: Waarde waar op gesorteerd dient te worden.
 
     Return:
-        Gesorteerde lijst.
+        • Gesorteerde lijst.
+        • Verstreken tijd.
+
     """
-    start_time = time.time()
+    start_time = time.time()#voor het bijhouden van de tijd
     if len(lst) > 1:
         #deel lijst op in twee delen
         mid = int(len(lst) // 2)
