fdbf1676e3939f524b7a5d208e418ebea933c6ef
h8f5y2 <144606226+h8f5y2@users.noreply.github.com>
2024-01-30 15:44:24 +0100

    Kleine aanpassingen

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from basisfuncties import data_naar_pandas#functie
 from basisfuncties import json_path#bestand
-from Statistiek_algoritmen.statistiek import plot_staafdiagram_rapportcijfers,plot_insight_ratings_per_game
+from Statistiek_algoritmen.statistiek import plot_staafdiagram_rapportcijfers,plot_inzicht_waardering_per_game
 from views import views
 
 
@@ -13,7 +13,7 @@ app.register_blueprint(views, url_prefix="/views")
 def home():
     df = data_naar_pandas(json_path)
     rapportcijfers_staafdiagram = plot_staafdiagram_rapportcijfers(df,'cijfer')
-    ratings_game = plot_insight_ratings_per_game(df,20)
+    ratings_game = plot_inzicht_waardering_per_game(df, 20)
     return render_template('home.html', rapportcijfers_staafdiagram=rapportcijfers_staafdiagram,ratings_game=ratings_game)
 
 
